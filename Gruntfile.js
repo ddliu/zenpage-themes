552b8efff674b38ef86977c97d63c91afddc8846
dong <ddliuhb@gmail.com>
2014-05-07 18:50:08 +0800

    Use bower to manage third party assets; use grunt to build project

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    less: {
+      options: {
+        // compress: true
+      },
+      all: {
+        expand: true,
+        cwd: "./",
+        src: ["*/*.less", "!_base/**/*"],
+        dest: "./",
+        ext: ".css",
+      }
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'nodeunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+
+  // Default task.
+  grunt.registerTask('default', ['less']);
+
+};
---
+++ README.md
@@ -0,0 +1,61 @@
+# zenpage-themes
+
+Theme collections for zenpage
+
+## Install
+
+```
+mkdir ~/.zenpage && cd ~/.zenpage
+git clone https://github.com/ddliu/zenpage-themes.git themes
+```
+
+## Make Theme
+
+A perticular theme structure looks like this:
+
+```
+theme_name/
+├── layout.html
+├── logo.png
+├── theme.css
+├── theme.js
+└── theme.json
+```
+
+### Layout
+
+`layout.html` is the layout template of the theme which is written in Mustache.
+
+### Template Variables
+
+- url.theme: Url to current theme, you can use it to locate js/css files.
+- meta: Metadata of currently rendered file.
+- TOC: Table of contents
+- config
+
+
+### theme.json
+
+theme.json contains information of the theme, here is an example:
+
+```
+{
+    "name": "simple",
+    "description": "Simple theme",
+    "version": "0.1.0",
+    "author": {
+        "name": "dong",
+        "email": "ddliuhb@gmail.com"
+    },
+    "license": "MIT"
+}
+```
+
+## Build
+
+```
+npm install -g grunt-cli
+bower install
+npm install
+grunt
+```
